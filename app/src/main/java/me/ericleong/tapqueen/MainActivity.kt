c165db6ecd9dd93da5fb24d54ca34d5bbf1a5ae3
Eric Leong <me@ericleong.me>
2017-05-30 03:46:34 -0400

    Initial commit.

---
+++ app/src/main/java/me/ericleong/tapqueen/MainActivity.kt
@@ -0,0 +1,68 @@
+package me.ericleong.tapqueen
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.SystemClock
+import android.support.v7.app.AppCompatActivity
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.TextView
+import android.widget.Toast
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    var started = false
+    var count = 0
+    var high = 0
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val highScore = findViewById(R.id.highScore) as TextView
+        highScore.text = resources.getString(R.string.high_score, 0)
+
+        val button = findViewById(R.id.button) as Button
+
+        val chronometer = findViewById(R.id.chronometer) as Chronometer
+        chronometer.isCountDown = true
+
+        val gestureListener = object : GestureDetector.SimpleOnGestureListener() {
+            override fun onDown(e: MotionEvent?): Boolean {
+                if (!started) {
+                    started = true
+                    chronometer.base = SystemClock.elapsedRealtime() + TimeUnit.SECONDS.toMillis(3)
+                    chronometer.start()
+                } else {
+                    count++
+                }
+
+                button.text = count.toString()
+
+                return true
+            }
+        }
+        val gestureDetector = GestureDetector(this, gestureListener)
+
+        button.setOnTouchListener { _, event -> gestureDetector.onTouchEvent(event) }
+
+        chronometer.setOnChronometerTickListener { c ->
+            if (SystemClock.elapsedRealtime() >= c.base) {
+                started = false
+                c.stop()
+                button.setText(R.string.start)
+                Toast.makeText(this, count.toString(), Toast.LENGTH_SHORT).show()
+                if (count > high) {
+                    high = count
+
+                    highScore.text = resources.getString(R.string.high_score, count)
+                }
+                count = 0
+            }
+        }
+    }
+}
